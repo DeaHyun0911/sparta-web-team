9d0c1582c18dd6c38ba32b15c720fdceb875f10c
최대현 <eogus0911@gmail.com>
2024-11-05 08:59:45 +0900

    파이어베이스 모듈 추가

---
+++ src/modules/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG2IzmkAHSNV_G4_38AU7z1-4FPNuZFm8",
+  authDomain: "sparta-web-team.firebaseapp.com",
+  projectId: "sparta-web-team",
+  storageBucket: "sparta-web-team.firebasestorage.app",
+  messagingSenderId: "861376660551",
+  appId: "1:861376660551:web:7def49e256024e7b8a5d62",
+  measurementId: "G-2W4FV36RSN"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs };
